c0c1abb3137e97ceadc333e9349c4f141a2fb40d
ShuvoHassan <shuvo.hassan@hotmail.com>
2020-02-10 23:33:47 +0600

    Add reducer & input form

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,9 +1,89 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addLead } from '../../actions/leads';
 
 class Form extends Component {
+  state = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  static propTypes = {
+    addLead: PropTypes.func.isRequired
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { name, email, message } = this.state;
+    const lead = { name, email, message };
+    this.props.addLead(lead);
+  };
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
-    return <h1>Form</h1>;
+    const { name, email, message } = this.state;
+    return (
+      <div>
+        <h2>Input Form</h2>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              aria-describedby="helpId"
+              value={name}
+              onChange={this.onChange}
+            />
+            <small id="helpId" className="form-text text-muted">
+              Please enter your full name
+            </small>
+          </div>
+          <div className="form-group">
+            <label>Email address</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              value={email}
+              onChange={this.onChange}
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label>Message</label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              name="message"
+              aria-describedby="helpId"
+              value={message}
+              onChange={this.onChange}
+            ></textarea>
+            <small id="helpId" className="form-text text-muted">
+              Please enter your message
+            </small>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Form;
+export default connect(null, { addLead })(Form);
--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,9 +1,57 @@
-import React, { Component } from 'react';
-
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getLeads, deleteLead } from '../../actions/leads';
 class Leads extends Component {
+  static propTypes = {
+    leads: PropTypes.array.isRequired,
+    getLeads: PropTypes.func.isRequired,
+    deleteLead: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    this.props.getLeads();
+  }
+
   render() {
-    return <h1>Leads List</h1>;
+    return (
+      <Fragment>
+        <h2 style={{ marginTop: 20 }}>Lead</h2>
+        <table className="table table-striped table-inverse table-responsive">
+          <thead className="thead-inverse">
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.leads.map(lead => (
+              <tr key={lead.id}>
+                <td scope="row">{lead.id}</td>
+                <td>{lead.name}</td>
+                <td>{lead.email}</td>
+                <td>{lead.message}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={this.props.deleteLead.bind(this, lead.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
+    );
   }
 }
 
-export default Leads;
+const mapStateToProps = state => ({
+  leads: state.leads.leads
+});
+
+export default connect(mapStateToProps, { getLeads, deleteLead })(Leads);
